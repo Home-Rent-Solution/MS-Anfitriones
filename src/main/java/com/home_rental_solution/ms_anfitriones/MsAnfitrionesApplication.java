88718e39bb887e8a3a05bccb92bb4d29c3ccf5a2
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-08 12:04:01 -0400

    Creacion del package model

---
+++ src/main/java/com/home_rental_solution/ms_anfitriones/MsAnfitrionesApplication.java
@@ -0,0 +1,13 @@
+package com.home_rental_solution.ms_anfitriones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAnfitrionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAnfitrionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home_rental_solution/ms_anfitriones/MsAnfitrionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home_rental_solution.ms_anfitriones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAnfitrionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
