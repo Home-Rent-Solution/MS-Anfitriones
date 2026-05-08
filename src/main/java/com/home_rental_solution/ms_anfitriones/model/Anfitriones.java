1ac021d4fa857ece2bb7269fc012d0729b2957fe
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-08 12:07:46 -0400

    Se implementó la clase de modelo Anfitriones: - Añadida entidad Anfitriones con atributos básicos. - Implementadas validaciones de Jakarta (@NotBlank, @Email, @NotNull). - Integrado Lombok para manejo automático de boilerplate (Data, Constructor).

--- src/main/java/com/home_rental_solution/ms_anfitriones/model/Anfitriones.java
+++ src/main/java/com/home_rental_solution/ms_anfitriones/model/Anfitriones.java
@@ -1,4 +1,29 @@
 package com.home_rental_solution.ms_anfitriones.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Anfitriones {
+
+    @NotNull(message = "El ID del anfitrion no debe estar vacio")
+    private Integer idAnfitrion;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ingresar un email valido")
+    private String email;
+
+    @NotBlank(message = "El telefono es obligatorio")
+    private String telefono;
+
+    private boolean verificado;
 }
