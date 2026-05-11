1aaad990ebcfd7b086b0b467a300642732ba0e7d
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-11 13:44:59 -0400

    Se mejora capa de datos en repository: - Migrado findByEmail a Optional para un manejo seguro de nulos. - Añadido existsByEmailIgnoreCase para validaciones de registro optimizadas. - Implementados filtros de búsqueda por estado de verificación (True/False)

--- src/main/java/com/home_rental_solution/ms_anfitriones/repository/AnfitrionesRepository.java
+++ src/main/java/com/home_rental_solution/ms_anfitriones/repository/AnfitrionesRepository.java
@@ -4,13 +4,23 @@ import com.home_rental_solution.ms_anfitriones.model.Anfitriones;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AnfitrionesRepository extends JpaRepository<Anfitriones, Integer> {
 
     //***EXTRAS***
     //buscar por email
-    Anfitriones findByEmailIgnoreCase(String email);
+    Optional<Anfitriones> findByEmailIgnoreCase(String email);
+
+    //verificar si el email ya existe
+    boolean existsByEmailIgnoreCase(String email);
 
     //validar si esta verificado
     boolean existsByIdAnfitrionAndVerificadoTrue(Integer idAnfitrion);
+
+    //Listar por estado de verificacion
+    List<Anfitriones> findByVerificadoTrue();
+    List<Anfitriones> findByVerificadoFalse();
 }
